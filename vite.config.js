fe1dab81e019653c02d1a78dc2a26d37c94ca25a
unknown <dipakr.gargote2001@gmail.com>
2025-12-28 17:14:17 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Dipak_Gargote_Portfolio",
+  base:process.env.VITE_BASE_PATH || "/Dipak_Gargote_Portfolio"
 })
